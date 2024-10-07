8fe6e747e6026661e525875d6c06767d201fa465
Matthew Stroud <mast224@student.oaisd.org>
2024-10-07 13:03:12 +0000

    Did some C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My Name is Matt Stroud");
+
+        //Using Variables
+        int age = 18;
+        string greeting = "Hello World";
+        string firstname = "Matt";
+        string lastname = "Stroud";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
